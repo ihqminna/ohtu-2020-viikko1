45a7b6a4d50f70d6f4c7f0df4dd4215fe54e0ac2
Minna Iholin <ihqminna@helsinki.fi>
2020-11-09 21:36:37 +0200

    riko koodi

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -61,7 +61,7 @@ public class Varasto {
         }
     }
 
-    public double otaVarastosta(double maara) {
+    public double otaVarastostaRiko(double maara) {
         if (maara < 0) // virhetilanteessa voidaan tehdä 
         {
             return 0.0;   // tällainen pikapoistuminenkin!
